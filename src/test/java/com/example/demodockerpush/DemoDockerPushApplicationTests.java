190e7e58a2ba7f31a3c076b1e931ae4513bce700
Momar diagne <momar.diagnee@gmail.com>
2023-09-17 15:22:19 +0200

    first commit

---
+++ src/test/java/com/example/demodockerpush/DemoDockerPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodockerpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
